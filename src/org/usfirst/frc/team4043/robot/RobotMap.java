f362667666ba1693f3f71fece8b6fde67dae7e6f
EthanHans3n <1ethanhansen@gmail.com>
2017-01-26 21:20:42 -0800

    Created Drive Train subsystem

--- src/org/usfirst/frc/team4043/robot/RobotMap.java
+++ src/org/usfirst/frc/team4043/robot/RobotMap.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team4043.robot;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -16,4 +20,22 @@ public class RobotMap {
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
+	public static CANTalon driveTrainCANTalonFR;
+	public static CANTalon driveTrainCANTalonFL;
+	public static CANTalon driveTrainCANTalonBR;
+	public static CANTalon driveTrainCANTalonBL;
+	
+	public static void init() {
+		driveTrainCANTalonFR = new CANTalon(1);
+		LiveWindow.addActuator("DriveTrain", "CAN Talon 1", driveTrainCANTalonFR);
+		
+		driveTrainCANTalonFL = new CANTalon(2);
+		LiveWindow.addActuator("DriveTrain", "CAN Talon 2", driveTrainCANTalonFL);
+		
+		driveTrainCANTalonBR = new CANTalon(3);
+		LiveWindow.addActuator("DriveTrain", "CAN Talon 3", driveTrainCANTalonBR);
+		
+		driveTrainCANTalonBL = new CANTalon(4);
+		LiveWindow.addActuator("DriveTrain", "CAN Talon 4", driveTrainCANTalonBL);
+	}
 }
---
+++ src/org/usfirst/frc/team4043/robot/subsystems/DriveTrain.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team4043.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+	
+	
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
